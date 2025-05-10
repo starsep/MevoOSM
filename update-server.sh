20989816b36d6e3f10c70d9d452c58e3c43ccbe0
Filip Czaplicki <github@starsep.com>
2025-05-10 18:42:02 +0200

    fix uv

--- update-server.sh
+++ update-server.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 set -eu
 date=$(/bin/date '+%Y%m%d')
-pip install -r requirements.txt
 git clone https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/starsep/MevoOSM --depth 1 --branch gh-pages output
 rm -rf cache/overpass
-python main.py
+uv run python main.py
 cd output
 git config user.name "MevoOSMBot"
 git config user.email "<>"
