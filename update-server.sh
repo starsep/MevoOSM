05844b6b1097b90d2bea6c48e5f681be61d08aed
Filip Czaplicki <github@starsep.com>
2025-05-10 20:53:29 +0200

    uv run --no-dev --no-progress

--- update-server.sh
+++ update-server.sh
@@ -3,7 +3,7 @@ set -eu
 date=$(/bin/date '+%Y%m%d')
 git clone https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/starsep/MevoOSM --depth 1 --branch gh-pages output
 rm -rf cache/overpass
-uv run python main.py
+uv run --no-dev --no-progress python main.py
 cd output
 git config user.name "MevoOSMBot"
 git config user.email "<>"
